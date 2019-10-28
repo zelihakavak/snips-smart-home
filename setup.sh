b630ed2a37d2c8fa1116980f2a6a7cfd09092d40
zelihakavak <zelihaycel@Gmail.com>
2019-10-28 15:57:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,19 @@
   
-#!/usr/bin/env bash
-set -e
-
-# Copy config.ini.default if it exists
-# Copy config.ini doesn't config.ini.default exist.
-if [ -e config.ini.default ] && [ ! -e config.ini ]; then
-    cp config.ini.default config.ini
-    chmod a+w config.ini
-fi
-
-PYTHON=$(command -v python3)
 VENV=venv
 
-if [ -f "$PYTHON" ]; then
+if [ ! -d "$VENV" ]
+then
 
-    if [ ! -d $VENV ]; then
-        # Virtual environment create if doesn't exist.
-        $PYTHON -m venv $VENV
+    PYTHON=`which python2`
+
+    if [ ! -f $PYTHON ]
+    then
+        echo "could not find python"
     fi
+    virtualenv -p $PYTHON $VENV
 
-    # Virtual environment activation; Install requirements.
-    # shellcheck disable=SC1090
-    . $VENV/bin/activate
-    pip3 install wheel
-    pip3 install -r requirements.txt
-
-else
-    >&2 echo "Cannot find Python 3. Please install it."
 fi
+
+. $VENV/bin/activate
+
+pip install -r requirements.txt
